a12925ca49d7ea76f7bfe2f4cc60f62c526687d7
Shivraj Desai <86651361+on-sd@users.noreply.github.com>
2021-09-11 23:33:47 +0530

    Update click.js

--- index/click.js
+++ index/click.js
@@ -2,14 +2,15 @@ setTimeout(() => {
     document.getElementById("start").style.display = "";
 }, 4000);
 var runapp=document.getElementById("runapps"),applist=document.getElementById("applist");
-function showrunapp(){runapp.style.display="block"}function hiderunapp(){runapp.style.display="none"}function showapplist(){applist.style.display="block"}function hideapplist(){applist.style.display="none"}
+// function showrunapp(){runapp.style.display="block"}function hiderunapp(){runapp.style.display="none"}
+function showapplist(){applist.style.display="block"}function hideapplist(){applist.style.display="none"}
 $(".ionspot").click(function(){ 
     hideapplist();
-    showrunapp();
+  //  showrunapp();
     $("#body").load("https://on-sd.github.io/files/onspot/index.html");
 });
 $(".iinstaller").click(function(){ 
     hideapplist();
-    showrunapp();
+  //  showrunapp();
     $("#body").load("https://on-sd.github.io/files/installer/index.html");
 });
