fbe22973f732f17a7e9dca41f4d3688dd9c58624
Shivraj Desai <86651361+on-sd@users.noreply.github.com>
2021-09-13 22:01:33 +0530

    Add files via upload

--- index/click.js
+++ index/click.js
@@ -1,19 +1,11 @@
 var back = document.getElementById("start");
 setTimeout(()=>{hidelogo()},1800);
-// var runapp=document.getElementById("runapps"),applist=document.getElementById("applist");
-// function showrunapp(){runapp.style.display="block"}function hiderunapp(){runapp.style.display="none"}
 function showlogo(){back.style.display="block"}function hidelogo(){back.style.display="none"}
 $(".ionspot").click(function(){ 
- //   showlogo();
-    // showrunapp();
     $("#runapp").load("https://on-sd.github.io/files/onspot/index.html");
     uninit();
-  //  setTimeout(()=>{hidelogo()},5000);
 });
 $(".iinstaller").click(function(){ 
-  //  showlogo();
-    // showrunapp();
     $("#runapp").load("https://on-sd.github.io/files/installer/index.html");
     uninit();
-   // setTimeout(()=>{hidelogo()},1500);
 });
